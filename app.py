8bd311481969ab0df5d7571f327f496b3604a4b3
Pirumohan Mandal <140044323+MohanPiru@users.noreply.github.com>
2024-02-02 01:18:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template      
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, world!'
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def home():
+    return render_template("home.html")
+    
+@app.route("/admin")
+def salvador():
+    return "Hello, Pirumohan"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
